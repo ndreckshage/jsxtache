1aa62872b0989a7d5648a9cff464e2265c06e498
nick dreckshage <nicholasdreckshage@gmail.com>
2014-11-12 17:41:25 -0500

    uncommenting tests

--- __tests__/partials-test.js
+++ __tests__/partials-test.js
@@ -5,71 +5,71 @@ var transform = require('./../src/jsxtache');
 var render = require('./../__helpers__/render');
 
 describe('jsxtache handles partials', function() {
-  // it('handles partials', function() {
-  //   var code = [
-  //     "var React = require('react');",
-  //     "module.exports = React.createClass({",
-  //     "  render: function() {",
-  //     "    return (",
-  //     "      '<div>{{> partial_a}}</div>'",
-  //     "    );",
-  //     "  }",
-  //     "});"
-  //   ].join('\n');
-  //
-  //   var codePartial = [
-  //     "var React = require('react');",
-  //     "module.exports = React.createClass({",
-  //     "  render: function() {",
-  //     "    return (",
-  //     "      '<p>Hello</p>'",
-  //     "    );",
-  //     "  }",
-  //     "});"
-  //   ].join('\n');
-  //
-  //   var result = transform(code);
-  //   var partial = transform(codePartial);
-  //
-  //   jest.setMock('partial_a', eval(partial.js));
-  //   var mustachePartials = { 'partial_a': partial.mustache };
-  //
-  //   var rendered = render(result, {}, mustachePartials);
-  //   expect(rendered.mustache).toEqual(rendered.react);
-  // });
-  //
-  // it('handles partials with context', function() {
-  //   var code = [
-  //     "var React = require('react');",
-  //     "module.exports = React.createClass({",
-  //     "  render: function() {",
-  //     "    return (",
-  //     "      '<div>{{#this.props.arr}}{{> partial_a}}{{/this.props.arr}}</div>'",
-  //     "    );",
-  //     "  }",
-  //     "});"
-  //   ].join('\n');
-  //
-  //   var codePartial = [
-  //     "var React = require('react');",
-  //     "module.exports = React.createClass({",
-  //     "  render: function() {",
-  //     "    return (",
-  //     "      '<p>{{name}}</p>'",
-  //     "    );",
-  //     "  }",
-  //     "});"
-  //   ].join('\n');
-  //
-  //   var result = transform(code);
-  //   var partial = transform(codePartial);
-  //
-  //   jest.setMock('partial_a', eval(partial.js));
-  //   var mustachePartials = { 'partial_a': partial.mustache };
-  //
-  //   var rendered = render(result, { name: 'BAD', arr: [{ name: 'Nick' }, { name: 'Hello' }, { name: 'Goodbye' }]}, mustachePartials);
-  //   expect(rendered.mustache).toEqual(rendered.react);
-  // });
+  it('handles partials', function() {
+    var code = [
+      "var React = require('react');",
+      "module.exports = React.createClass({",
+      "  render: function() {",
+      "    return (",
+      "      '<div>{{> partial_a}}</div>'",
+      "    );",
+      "  }",
+      "});"
+    ].join('\n');
+
+    var codePartial = [
+      "var React = require('react');",
+      "module.exports = React.createClass({",
+      "  render: function() {",
+      "    return (",
+      "      '<p>Hello</p>'",
+      "    );",
+      "  }",
+      "});"
+    ].join('\n');
+
+    var result = transform(code);
+    var partial = transform(codePartial);
+
+    jest.setMock('partial_a', eval(partial.js));
+    var mustachePartials = { 'partial_a': partial.mustache };
+
+    var rendered = render(result, {}, mustachePartials);
+    expect(rendered.mustache).toEqual(rendered.react);
+  });
+
+  it('handles partials with context', function() {
+    var code = [
+      "var React = require('react');",
+      "module.exports = React.createClass({",
+      "  render: function() {",
+      "    return (",
+      "      '<div>{{#this.props.arr}}{{> partial_a}}{{/this.props.arr}}</div>'",
+      "    );",
+      "  }",
+      "});"
+    ].join('\n');
+
+    var codePartial = [
+      "var React = require('react');",
+      "module.exports = React.createClass({",
+      "  render: function() {",
+      "    return (",
+      "      '<p>{{name}}</p>'",
+      "    );",
+      "  }",
+      "});"
+    ].join('\n');
+
+    var result = transform(code);
+    var partial = transform(codePartial);
+
+    jest.setMock('partial_a', eval(partial.js));
+    var mustachePartials = { 'partial_a': partial.mustache };
+
+    var rendered = render(result, { name: 'BAD', arr: [{ name: 'Nick' }, { name: 'Hello' }, { name: 'Goodbye' }]}, mustachePartials);
+    expect(rendered.mustache).toEqual(rendered.react);
+  });
 
   it('passes down state for client side interaction', function() {
     var code = [
